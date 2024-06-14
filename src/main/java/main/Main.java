606b2abc766195e3893a5220dc973de946ea2fc5
Alice Dev <imalicedev@gmail.com>
2024-06-14 17:43:23 -0300

    Estudos sobre árvore binária de busca

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,10 +1,19 @@
 package main;
 
-import dataStructures.Queue;
-import dataStructures.Tree;
+import dataStructures.BST;
 
 public class Main {
     public static void main(String[] args) {
+        BST tree = new BST();
+        tree.insert(37);
+        tree.insert(66);
+        tree.insert(42);
+        tree.insert(11);
+        tree.insert(72);
+        tree.insert(8);
+        tree.insert(17);
 
+        tree.deleteNode(8);
+        tree.inOrder();
     }
 }
